5fef5717be0b84eb7fbc6bde499c43407e747b8d
fomer07 <omerf.ay7@gmail.com>
2021-02-22 15:08:57 +0300

    added cors configuration to allow JS requests

---
+++ src/main/java/com/frank/basicblog/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.frank.basicblog.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
